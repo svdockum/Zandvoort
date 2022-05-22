dd0f5641a5dfd891a7a9948d41f37fe0e2091f69
Stefan van Dockum <stefan@vandockum.nl>
2022-05-22 22:02:00 +0200

    csotmer model

--- app/Customer.php
+++ app/Customer.php
@@ -14,7 +14,7 @@ use SoftDeletes;
      protected $dates = ['deleted_at'];
 
 protected $fillable = [
-        'name', 'email1','contactperson', 'phone1','phone2', 'street','housenumber', 'postalcode', 'city','isKeerBlus','isNood','can_login'
+        'name', 'email1','contactperson', 'phone1','phone2', 'street','housenumber', 'postalcode', 'city','isKeerBlus','isNood','can_login','isAlarm','isBMI'
     ];
 
      public function locations()
